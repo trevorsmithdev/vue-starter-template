3968a2ccb2d2c43eff2e26bdb27fea6d2edb03b6
trevorsmithdev <trevor.smith.dev@gmail.com>
2021-03-06 12:09:21 -0600

    add demo todo component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <h1 class="hello-world">{{ message }}</h1>
+  <ToDoList />
 </template>
 
 <script>
+import ToDoList from './components/demo/ToDoList.vue'
 export default {
+    components: {
+      ToDoList
+    },
     data () {
         return {
             message: 'Hello World'
---
+++ src/components/demo/ToDoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <div class="input-wrapper">
+          <input v-model="todo" class="todo-input" />
+          <button v-on:click="addTodo">Add ToDo</button>
+      </div>
+      <ul>
+          <li v-for="(item, index) in todos" v-bind:key="index">{{ item }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            todos: [],
+            todo: null
+        }
+    },
+    methods: {
+        addTodo () {
+            if (this.todo) {
+                this.todos.push(this.todo)
+                this.todo = null
+            }
+            
+        }
+    }
+
+}
+</script>
+
+<style>
+.input-wrapper {
+    display: flex;
+    justify-content: center;
+}
+
+.todo-input {
+    margin-right: 10px;
+}
+
+
+</style>
