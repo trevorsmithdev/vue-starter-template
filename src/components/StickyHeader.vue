99f0e5babf0b1adebf6cbcec746b043104331780
trevorsmithdev <trevor.smith.dev@gmail.com>
2021-03-15 23:57:04 -0500

    sticky header wip

---
+++ src/components/StickyHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="sticky-header" class="flex items-center p-3">
+      <h1 class="text-3xl">Sticky Header</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss">
+  #sticky-header {
+      height: 11vh;
+      background-color: #605856;
+  }
+</style>
